a2d5764d8ec2823305757868f3b2d304d7ee0056
professionaltechs <115035072+professionaltechs@users.noreply.github.com>
2024-04-16 23:05:05 +0500

    games

---
+++ src/components/Home/GameCards.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const GameCards = () => {
+  return (
+    <div
+      className={`bg-[#1272E0] px-4 py-4 w-full max-w-[400px] text-white rounded-lg shadow-2xl lg:py-7 lg:px-6 `}
+    >
+      <div className="flex justify-between items-center">
+        <h3 className="text-2xl font-semibold">Pick 1</h3>
+        <p className="py-1 px-4 border border-white rounded-2xl">AED 5</p>
+      </div>
+      <p className="mt-7 lg:mt-9 text-center ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,{" "}
+      </p>
+      <div className="flex justify-between gap-2 mt-5 lg:mt-7">
+        <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 w-full max-w-[300px] mx-auto">
+          <div className="bg-white text-black text-center rounded-[8px] pt-1">
+            <p className="text-[18px] lg:text-[20px] font-semibold">00</p>
+            <p className="text-[11px] lg:text-[12px]">Days</p>
+          </div>
+          <div className="bg-white text-black text-center rounded-[8px] pt-1">
+            <p className="text-[18px] lg:text-[20px] font-semibold">00</p>
+            <p className="text-[11px] lg:text-[12px]">Hours</p>
+          </div>
+          <div className="bg-white text-black text-center rounded-[8px] pt-1">
+            <p className="text-[18px] lg:text-[20px] font-semibold">00</p>
+            <p className="text-[11px] lg:text-[12px]">Minutes</p>
+          </div>
+          <div className="bg-white text-black text-center rounded-[8px] pt-1">
+            <p className="text-[18px] lg:text-[20px] font-semibold">00</p>
+            <p className="text-[11px] lg:text-[12px]">Seconds</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,23 @@ import React, { useState } from "react";
 import Carousel from "../components/Home/Carousel";
 import LatestDraws from "../components/Home/LatestDraws";
 import Stories from "../components/Home/Stories";
+import GameCards from "../components/Home/GameCards";
 
 const Home = () => {
-  const [abc, def] = useState(Carousel)
-
   return (
     <div className="relative">
       <Carousel />
       <LatestDraws />
       <div className="ourGames mt-24 px-2 lg:px-10">
-        <h1 className="text-3xl text-center lg:text-5xl font-semibold">Our Games</h1>
-          
+        <h1 className="text-3xl text-center lg:text-5xl font-semibold">
+          Our Games
+        </h1>
+        <div className="flex flex-wrap justify-center gap-9 mt-10 lg:mt-20">
+          <GameCards />
+          <GameCards />
+          <GameCards />
+          <GameCards />
+        </div>
       </div>
       <Stories />
     </div>
